621f9209bf063b8b9257b42f2b1b5f9c202736e0
GimYoungPhil <youngphil.gim@gmail.com>
2019-11-22 16:47:50 +0900

    a

---
+++ src/test/test.ts
@@ -0,0 +1,12 @@
+interface LabeledValue {
+  label: string;
+}
+
+function printLabel(labeledObj: LabeledValue) {
+  console.log(labeledObj.label);
+}
+
+let myObj = { size: 10, label: "Size 10 Object" };
+printLabel(myObj);
+
+printLabel({ label: "Hi Hello", greeting: 10 });
